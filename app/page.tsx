c3d6f52766b9d59a2fbfa5b782099f04919c7aee
Rupesh Khadka <rupeshkhadka000@gmail.com>
2025-04-04 19:03:04 +0545

    new pro

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="text-7xl text-primary">Hello</h1>
+      <Button className="bg-primary text-white">Click me</Button>
     </>
   );
 }
