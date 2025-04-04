f2f32f7e3b90f77d7ff1a14b7a6f3323b7b227f0
Rupesh Khadka <rupeshkhadka000@gmail.com>
2025-04-04 19:22:18 +0545

    fixing ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-7xl text-primary">Hello</h1>
       <Button className="bg-primary text-white">Click me</Button>
-    </>
+    </div>
   );
 }
